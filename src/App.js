3d93a30d3ebb8c1b6fbb3405e6099ce19878221a
Fred <frederic@pmo.digital>
2020-01-20 18:42:10 +0100

    Etape 16bis

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import Titre from './Titre';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
 
-  state = {
-    nom1: 'Bob',
-    nom2: 'Marion',
-    nom3: 'Sophie'
-  }
-
   render() {
     return (
       <div className="App">
-        <Titre nom={this.state.nom1}  />
-        <Titre nom={this.state.nom2} />
-        <Titre nom={this.state.nom3} />
+        Hello
       </div>
     );
   }
