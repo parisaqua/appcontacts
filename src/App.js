a577a2a62b70f1472d6fee3d173c7ab28c1072de
Frédéric BASTIAN <frederic@bastian.paris>
2020-01-20 15:47:35 +0100

    Etape 16

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Titre from './Titre';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
--- src/Titre.js
+++ src/Titre.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import './Titre.css'
 
 function Titre(props) {
     return (
         <div>
             <h1>Hi, my name is {props.nom}.</h1>
+            <p className='text-warning'>Mon texte</p>
         </div>
     )
 }
